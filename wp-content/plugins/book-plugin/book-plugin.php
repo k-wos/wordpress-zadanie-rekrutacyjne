3d63521ef64354513d2a1615d23054bbaa532fb3
k-wos <kacper.wos33@gmail.com>
2024-02-29 14:24:00 +0100

    Init commit

---
+++ wp-content/plugins/book-plugin/book-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+Plugin Name: Book Plugin
+Plugin URI: localhost
+Description: Wtyczka stworzona na potrzeby zadania rekrutacyjnego
+Version: 1.0
+Author: Kacper Woś
+Author URI: localhost
+*/
+?>
